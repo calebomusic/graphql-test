aad4970a1e440b87c6bbe8a7c762c4ffa0ee4992
calebomusic <calebomusic@gmail.com>
2017-07-29 08:17:10 -0700

    Finish express tutorial

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      { graphqlExpress, graphiqlExpress } = require('graphql-server-express'),
+      { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = [`
+type Query {
+    hello: String
+}
+
+schema {
+    query: Query
+}`];
+
+const resolvers = {
+    Query: {
+        hello(root) {
+            return "world";
+        }
+    }
+}
+
+const schema = makeExecutableSchema({typeDefs, resolvers});
+
+const app = express();
+
+app.use("/graphql", bodyParser.json(), graphqlExpress({schema}));
+app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
+app.listen(4000, () => console.log("Now browse to localhost:4000/graphiql"));
